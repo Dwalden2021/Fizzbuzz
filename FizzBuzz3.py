3f9f57a7f27ba0077b82254d154f35f229744587
Dwalden2021 <92900193+Dwalden2021@users.noreply.github.com>
2022-06-26 10:22:37 -0400

    Update FizzBuzz3.py

--- FizzBuzz3.py
+++ FizzBuzz3.py
@@ -1,20 +1,25 @@
-#FizzBuzz
-
+#FizzBuzz_3
 def fizz_buzz(number):
-    answer = ''
+    fizz_or_buzz = '' #Response Value
+
     if number % 3 == 0 and number % 5 == 0:
-        answer = 'FizzBuzz'
+        fizz_or_buzz = 'FizzBuzz'
     elif number % 3 == 0:
-        answer = 'Fizz'
+        fizz_or_buzz = 'Fizz'
     elif number % 5 == 0:
-        answer = 'Buzz'
+        fizz_or_buzz = 'Buzz'
     else:
-        answer = number
-    return answer
+        fizz_or_buzz = number
+    return fizz_or_buzz
 
 def main():
+    #Setting up the range to itterate through
     for x in range(1,101):
-        answer = fizz_buzz(x)
-        print(answer)
+        fizz_or_buzz = fizz_buzz(x)
+        #line up the messages
+        if len(str(x)) > 2 :
+            print(x,":   ",fizz_or_buzz,sep="")
+        else:
+            print(x,":    ",fizz_or_buzz,sep="")
 
 main()
